0589061f201fafdabf5b09c33f3ea432e76638b3
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2025-06-29 21:08:22 +0300

    Fixed MOF environment

--- tdmpc2/envs/mof/wrappers/pixels.py
+++ tdmpc2/envs/mof/wrappers/pixels.py
@@ -6,10 +6,12 @@ from typing import Tuple, Union
 
 
 class Pixels(gym.Wrapper):
-    def __init__(self, env: gym.Env, image_size: Union[Tuple[int, int], int]) -> None:
+    def __init__(self, env: gym.Env, image_size: Union[Tuple[int, int], int], normalize_image: bool = False) -> None:
         super().__init__(env)
+        self.normalize_image = normalize_image
         self.image_size = (image_size, image_size) if isinstance(image_size, int) else image_size
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(3,) + self.image_size, dtype=float)
+        shape = (3,) + self.image_size if self.normalize_image else self.image_size + (3,)
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=shape, dtype=float)
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, dict]:
         _, reward, done, info = self.env.step(action)
@@ -25,7 +27,11 @@ class Pixels(gym.Wrapper):
         else:
             image = Image.fromarray(self.env.render(mode='rgb_array'))
             image = np.array(image.resize(self.image_size))
-        return np.moveaxis(image, -1, 0) / 255.0
+
+        if self.normalize_image:
+            return np.moveaxis(image, -1, 0) / 255.0
+
+        return image.copy()
 
     def render(self, mode='human', **kwargs):
         size = self.image_size
